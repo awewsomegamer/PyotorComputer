e1939c6d160eb8cdab9cec047a09716ff4016ed1
awewsomegamer <>
2023-05-14 02:52:39 -0700

    llvm-mos
    I have switched to this compiler, and it was very simple and
    straight forward. The code also works.

--- kernal/kernal.c
+++ kernal/kernal.c
@@ -1,28 +1,27 @@
 #include <stdint.h>
 
-int cursor_index = 0;
-char *s = "Happy Mother's Day";
-char *h = "Boldog Anyak Napjat";
+uint16_t cursor_index = 0;
+char *a = "Happy Mother's Day ";
+char *h = "Boldog Anyak Napjat ";
 
-void draw_pixel(uint16_t address, uint8_t color) {
-	*((uint16_t *)48512) = address; // Address
+void draw_pixel(int x, int y, uint8_t color) {
+	*((uint16_t *)48512) = (uint16_t)(y * 320 + x); // Address
 	*((uint8_t *)48514) = 0x00; // Terminal Mode
 	*((uint8_t *)48515) = color; // Data
 	*((uint8_t *)48516) = 0xC0; // Status
 }
 
-void putc(char c) {
+void putc(char c, uint8_t no_bg) {
 	*((uint16_t *)48512) = cursor_index; // Address
 	*((uint8_t *)48514) = 0x03; // Terminal Mode
 	*((uint8_t *)48515) = c; // Data
-	*((uint8_t *)48516) = 0b11010000; // Status
+	*((uint8_t *)48516) = 0b11000000 | ((no_bg & 1) << 4); // Status
 	cursor_index++;
 }
 
-void puts(char *s) {
-	int i = 0;
-	for (i = 0; s[i] != 0; i++)
-		putc(s[i]);
+void puts(char *s, uint8_t no_bg) {
+	for (int i = 0; s[i] != 0; i++)
+		putc(s[i], no_bg);
 }
 
 void set_fg(char color) { *((uint8_t *)48517) = color; }
@@ -31,36 +30,15 @@ void set_bg(char color) { *((uint8_t *)48518) = color; }
 void NMI_HANDLER() { }
 void IRQ_HANDLER() { }
 
-
 int main() {
-	int x = 0;
+	for (int i = 0; i < 200; i++)
+		for (int j = 0; j < 320; j++)
+			draw_pixel(j, i, ((i * j) % 0xFF) << 5);
 
-	for (; x < 10; x++) {
-		int i = 0;
-		int j = 0;
-		
-		// for (; j < 200; j++)
-		// 	for (; i < 320; i++)
-		// 		draw_pixel(i, j, ((i + j) % 0xFF) << 5);
-		
-		// i = 0;
-
-		// for (; i < 20; i++) {
-		// 	set_fg(i << 5);
-		// 	puts(s);
-		// 	putc(' ');
-		// 	puts(h);
-		// 	putc(' ');
-		// }
-
-		i = 0;
-		for (; i < 1000; i = i + 1) {
-			draw_pixel(i, 0xFF);
-			int p = 0;
-			for (; p < 1000; p++);
-		}
-	}
-
+	set_fg(0xFF);
+	putc(' ', 1);
+	puts(a, 1);
+	puts(h, 1);
 
 	for (;;);
 
--- kernal/lib/vectors.s
+++
@@ -1,8 +0,0 @@
-.import _init
-.import _nmi_int, _irq_int
-
-.segment "VECTORS"
-
-.addr _nmi_int
-.addr _init
-.addr _irq_int
